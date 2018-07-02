e4ffca6325d39dad4c4e637c64c91011234bb336
Henrique Dias <diaslinoh@gmail.com>
2018-07-02 18:11:53 -0300

    Router Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+{path:'',component:HomeComponent}, 
+{path:'test',component:TestComponent}, 
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
